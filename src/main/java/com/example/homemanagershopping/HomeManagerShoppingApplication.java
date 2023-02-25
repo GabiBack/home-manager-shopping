bb805ec760b21498ccf81613035f002543017f88
Gabriela Knapik <s210964@student.uek.krakow.pl>
2023-02-25 12:28:41 +0100

    Initial commit

---
+++ src/main/java/com/example/homemanagershopping/HomeManagerShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.homemanagershopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeManagerShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeManagerShoppingApplication.class, args);
+    }
+
+}
